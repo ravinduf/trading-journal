316bd28d72d20766820d107ec02f07968132c523
ravinduf <ravinduf@rocketmail.com>
2025-12-01 00:38:33 +0530

    basic login page ui and added route to signup page

--- frontend/src/pages/login/Login.tsx
+++ frontend/src/pages/login/Login.tsx
@@ -1,11 +1,26 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { useNavigate } from "react-router-dom"
 
 const Login = () => {
+  const navigate = useNavigate();
   return (
-    <div className='text-primary-foreground'>Login
-        <Button>Hello</Button>
-    </div>
+    <>
+      <img src={"/login-bg.webp"} alt="bg" className='-z-10 absolute h-[100vh] bg-background'/>
+      <section className="mx-auto w-1/3 pt-20">
+        <div className="text-center my-6 text-3xl font-semibold">Login</div>
+        <div className="space-y-5">
+          <Input placeholder="Email"/>
+          <Input placeholder="Password" />
+        </div>
+        <Button className="w-full mt-8">Login</Button>
+        <div className="text-sm text-center my-4">
+          No Account? <Button variant="link" className="text-blue-700" onClick={() => {
+            navigate("/signup")
+          }}>Sign Up</Button>
+        </div>
+      </section>
+    </>
   )
 }
 
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "./pages/login/Login";
 import Home from "./pages/home/Home";
+import Signup from "./pages/signup/Signup";
 
 export const router = createBrowserRouter([
   { 
@@ -10,5 +11,9 @@ export const router = createBrowserRouter([
   {
     path: "/login",
     Component: Login
+  },
+  {
+    path: "/signup",
+    Component: Signup,
   }
 ]);
